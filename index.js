f141b5b3d7dac0eaa607e624c09749df72399609
Christian Felix <1christianfelix@gmail.com>
2022-10-19 06:02:19 -0700

    first commit
    first map creation and canvas set up

---
+++ index.js
@@ -0,0 +1,18 @@
+const canvas = document.querySelector("canvas");
+
+const c = canvas.getContext("2d");
+
+canvas.width = 1024;
+canvas.height = 576;
+
+c.fillStyle = "black";
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+const image = new Image();
+image.src = "/img/Room Map.png";
+
+console.log(image);
+
+image.onload = () => {
+  c.drawImage(image, 256, 64);
+};
