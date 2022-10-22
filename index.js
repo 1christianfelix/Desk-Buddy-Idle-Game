accefd1341db5b0747fbc048015a5dcf863ec44d
Christian Felix <1christianfelix@gmail.com>
2022-10-22 14:50:15 -0700

    Added Movemented System
    Implemented movement system under the player class then moved the function call to the animation loop rather than the windows event listener for smoother movement.

--- index.js
+++ index.js
@@ -10,23 +10,19 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 
 const image = new Image();
 
-console.log(image);
-
 const playerImage = new Image();
 
 // calling smaller item so that loading doesn't get interrupted by delay in larger item. Map might load in and get placed while player is loading, which will stop it.
 playerImage.src = "./char/char_down.png";
 image.src = "./img/Room Map.png";
 // onload is an event listener that executes a function when item is loaded
-console.log(playerImage.width, playerImage.height);
 
-////////////////////
-/*  PLAYER CLASS  */
-////////////////////
+/////////////////////////
+/*  PLAYER DEVELOPMENT */
+/////////////////////////
 
 class Player {
   constructor() {
-    let playerReference = this;
     // object for drawImage properties
     this.drawProperties = {
       moveFrame: ["x", "x", "x", "x"],
@@ -42,20 +38,44 @@ class Player {
       ["w", "./char/char_up.png"],
       ["s", "./char/char_down.png"],
     ]);
+  }
 
-    this.moveDirection = {
-      currentPosition: playerReference.drawProperties.position,
-      moveUp() {
-        console.log(this.currentPosition);
-      },
-    };
+  move(key) {
+    console.log("test");
+    let [x, y] = this.drawProperties.position;
+    let arr = [x, y];
+
+    if (keys[`${key}`].pressed) {
+      console.log("aaaa");
+
+      switch (key) {
+        case "w":
+          y -= 4;
+          arr = [x, y];
+          this.drawProperties.position = arr;
+          break;
+        case "a":
+          x -= 4;
+          arr = [x, y];
+          this.drawProperties.position = arr;
+          break;
+        case "s":
+          y += 4;
+          arr = [x, y];
+          this.drawProperties.position = arr;
+          break;
+        case "d":
+          x += 4;
+          arr = [x, y];
+          this.drawProperties.position = arr;
+          break;
+      }
+    }
+    console.log(this.drawProperties.position);
   }
 
   startingPoint(image, startFrame, startPosition, scale) {
-    // console.log("t", image);
-    // console.log(playerImage);
     this.drawProperties["image"] = image;
-    console.log("g ", this.drawProperties["image"]);
     this.drawProperties["moveFrame"] = startFrame;
     this.drawProperties["position"] = startPosition;
     this.drawProperties["scale"] = scale;
@@ -95,7 +115,6 @@ class Player {
   }
 }
 
-// Player initializing
 let playerStartFrame = [0, 0, 64, 96];
 let playerStartPosition = [676, 416];
 let player = new Player();
@@ -105,8 +124,6 @@ player.startingPoint(
   playerStartPosition,
   [64, 96]
 );
-
-player.moveDirection.moveUp();
 
 const keys = {
   w: {
@@ -123,76 +140,47 @@ const keys = {
   },
 };
 
-// console.log(player.direction.get("right"));
-// playerImage.src = player.direction.get("left");
-
+let lastKey = "";
 function animate() {
   window.requestAnimationFrame(animate);
-  // z-index position bottom to top
   c.drawImage(image, 256, 64);
-  // c.drawImage(playerImage, 196, 0, 64, 96, 676, 416, 64, 96);
   player.draw(player.drawParameters());
 
-  // if (keys.w.pressed) {
-  //   playerImage.src = player.direction.get("up");
-  // }
-  // if (keys.a.pressed) {
-  //   playerImage.src = player.direction.get("left");
-  // }
-  // if (keys.s.pressed) {
-  //   playerImage.src = player.direction.get("down");
-  // }
-  // if (keys.d.pressed) {
-  //   playerImage.src = player.direction.get("right");
-  // }
+  console.log("test");
+  let [x, y] = player.drawProperties.position;
+  let arr = [x, y];
+
+  if (/^[wasd]$/i.test(lastKey)) player.move(lastKey);
 }
 animate();
-console.log(player.drawProperties["position"]);
 
-console.log(keys);
+////////////////////////////
+/*  MOVEMENT DEVELOPMENT  */
+////////////////////////////
+
 window.addEventListener("keydown", (keyPressed) => {
-  console.log(keyPressed.key);
-  keys[`${keyPressed.key}`].pressed = true;
-  playerImage.src = player.faceDirection.get(`${keyPressed.key}`);
-  player.drawParameters;
-  console.log("key: ", keys[`${keyPressed.key}`]);
+  /*   
+    Only proceed checking if WASD was pressed
+    REGEX Explaination:
+    /pattern/flags
+    ^ first character contains
+    $ last character before newline contains
+    [] character class to search from 
+    wasd characters (not a string because of [])
+  */
+  if (/^[wasd]$/i.test(keyPressed.key)) {
+    keys[`${keyPressed.key}`].pressed = true;
+    if (keyPressed.key != lastKey) {
+      playerImage.src = player.faceDirection.get(`${keyPressed.key}`);
+      lastKey = `${keyPressed.key}`;
+    }
+    // player.move(keyPressed.key);
+  }
 });
 
 window.addEventListener("keyup", (keyPressed) => {
-  keys[`${keyPressed.key}`].pressed = false;
-  console.log("key: ", keys[`${keyPressed.key}`]);
+  if (/^[wasd]$/i.test(keyPressed.key)) {
+    keys[`${keyPressed.key}`].pressed = false;
+    // console.log("key: ", keys[`${keyPressed.key}`]);
+  }
 });
-
-// window.addEventListener("keydown", (keyPressed) => {
-//   switch (keyPressed.key) {
-//     case "w":
-//       keys.w.pressed = true;
-//       break;
-//     case "a":
-//       keys.a.pressed = true;
-//       break;
-//     case "s":
-//       keys.s.pressed = true;
-//       break;
-//     case "d":
-//       keys.d.pressed = true;
-//       break;
-//   }
-// });
-
-// window.addEventListener("keyup", (keyPressed) => {
-//   switch (keyPressed.key) {
-//     case "w":
-//       keys.w.pressed = false;
-//       break;
-//     case "a":
-//       keys.a.pressed = false;
-//       break;
-//     case "s":
-//       keys.s.pressed = false;
-//       break;
-//     case "d":
-//       keys.d.pressed = false;
-//       break;
-//   }
-// });
